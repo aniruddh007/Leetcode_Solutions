b9f38c89b16a3d9b9b5b93612c6c82e33c743208
Aniruddh Singh <90297183+aniruddh007@users.noreply.github.com>
2022-03-29 21:25:11 +0530

    Create 268-Missing-number.cpp

---
+++ 268-Missing-number.cpp
@@ -0,0 +1,28 @@
+"""
+problem link : https://leetcode.com/problems/missing-number/
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+"""
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        int sum1 = 0;
+        int sum2=0;
+        for(int i=0;i<n;i++)
+        {
+            sum1+=nums[i]; 
+        }
+        for(int i=0;i<=n;i++)
+        {
+            sum2+=i; 
+        }
+        return sum2-sum1; 
+    }
+};
